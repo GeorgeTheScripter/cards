15b85c6a163121ffc13bb4be4f0fba7961d1a5c9
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-09-08 08:41:33 +0300

    2-button

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Button from "./components/Button.vue";
+</script>
+
+<template>
+  <main class="card-field">
+    <Button>Начать игру</Button>
+  </main>
+</template>
+
+<style scoped>
+.card-field {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <button class="button"><slot /></button>
+</template>
+
+<style scoped>
+.button {
+  width: 335px;
+  height: 68px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 28px;
+  font-size: 24px;
+  color: var(--color-text-btn);
+  cursor: pointer;
+  background: var(--color-bg-accent);
+}
+
+.button:hover {
+  background: var(--color-bg-accent-hover);
+}
+</style>
